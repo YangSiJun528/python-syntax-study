0bbb26064451f1ea026a15c22b4d223d67aee2af
YangSijun528 <yangsijun5528@gmail.com>
2023-04-01 22:23:18 +0900

    1~10 완료

---
+++ basic_exam/print.py
@@ -0,0 +1,30 @@
+# 1
+print("Hello World")
+
+# 2
+print("Mary's cosmetics")
+
+# 3
+print("신씨가 소리질렀다. \"도둑이야\"")
+
+# 4
+print("C:\\Windows")
+
+# 5
+print("\\n : 줄바꿈, \\t tab")
+
+# 6
+print("Hello", "World", "Hello World")
+
+# 7
+print("naver", "kakao", "sk", "samsung", sep=";")
+
+# 8
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+# 9
+print("Hello World", end="")
+print("Hello World", end="\n")
+
+# 10
+print(5/3)
