ddc63b3fa094134a79061e444bf92a8ab0e7846c
YangSijun528 <yangsijun5528@gmail.com>
2023-04-02 21:47:29 +0900

    변수 11~20 완료

---
+++ basic_exam/variable.py
@@ -0,0 +1,46 @@
+# 11
+sam = 50000
+total = sam * 50
+print(total)
+
+# 12
+market_capitalization = 2.94e14
+current_price = 50000
+per = 15.79
+print(market_capitalization, current_price, per)
+
+# 13
+s = "hello"
+t = "python"
+print(s + "! " + t)
+
+# 14
+print("2 + 2 * 3 = ", 2 + 2 * 3, "=", 2 + 6)
+
+# 15
+a = "132"
+print(type(a), "=", "str")
+
+# 16
+num_str = "720"
+num_int = int("720")
+print(num_str, num_int)
+
+# 17
+num = 100
+print(str(num))
+
+# 18
+print(float("15.79"))
+
+# 19
+year = "2020"
+year = int(year)
+print(year - 1)
+print(year - 2)
+print(year - 3)
+
+# 20
+mon = 48584
+total_price = mon*36
+print(total_price)
