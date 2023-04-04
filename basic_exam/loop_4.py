42ccdfe72e47d7f7d72f84504fc4bb2538f36a57
YangSijun528 <yangsijun5528@gmail.com>
2023-04-04 23:57:01 +0900

    반복문 161~170 완료

---
+++ basic_exam/loop_4.py
@@ -0,0 +1,45 @@
+# 161
+for i in range(0, 100):
+    print(i + 1)
+
+# 162
+for i in range(2002, 2051, 4):
+    print(i)
+
+# 163
+for i in range(0, 30, 3):
+    print(i + 3)
+
+# 164
+for i in range(0, 100):
+    print(99 - i)
+
+# 165
+for i in range(0, 10):
+    print("%0.1f" % (0.1 * i))
+
+# 166
+for i in range(1, 10):
+    print("3x"+str(i)+" = "+str(i*3))
+
+# 167
+for i in range(1, 10, 2):
+    print("3x"+str(i)+" = "+str(i*3))
+
+# 168
+sum = 0
+for i in range(0, 10):
+    sum += (i+1)
+print(sum)
+
+# 169
+sum = 0
+for i in range(1, 11, 2):
+    sum += i
+print(sum)
+
+# 170
+sum = 1
+for i in range(1, 11):
+    sum *= i
+print(sum)
